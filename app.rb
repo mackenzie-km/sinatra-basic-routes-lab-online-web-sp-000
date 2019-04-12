89f0fc79f3a058f41309150939813adf0a68b940
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-12 21:50:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name'
+    "My name is __"
+  end
+
+  get '/hometown'
+    "My hometown is __"
+  end
+
+  get '/favorite-song'
+    "My favorite song is __"
+  end
 end
